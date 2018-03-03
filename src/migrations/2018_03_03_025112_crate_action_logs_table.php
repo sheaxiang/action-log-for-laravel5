f5af0b24de2dac6566ea24ab1b3b94d3f7b2e945
xiangxingyu <642290531@qq.com>
2018-03-03 15:45:05 +0800

    update

--- src/migrations/2018_03_03_025112_crate_action_logs_table.php
+++ src/migrations/2018_03_03_025112_crate_action_logs_table.php
@@ -3,21 +3,21 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrateActionLogTable extends Migration
+class CrateActionLogsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-    	if(!Schema::hasTable('action_logs')) {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if(!Schema::hasTable('action_logs')) {
 			Schema::create('action_logs', function (Blueprint $table) {
 				$table->increments('id');
-				$table->integer("user_id")->default(null)->comment("用户id");
-				$table->string("username")->comment("姓名");
-				$table->string("guard",20)->default(null);
+				$table->integer("user_id")->nullable()->default(null)->comment("用户id");
+				$table->string("username")->default(null)->comment("姓名");
+				$table->string("guard",20)->nullable()->default(null);
 				$table->string("type",50)->comment("操作类型");
 				$table->ipAddress("ip")->comment("操作者ip");
 				$table->string("browser",150)->comment("浏览器");
@@ -28,15 +28,15 @@ class CrateActionLogTable extends Migration
 				$table->timestamps();
 			});
 		}
-    }
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('action_logs');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('action_logs');
+	}
 }
